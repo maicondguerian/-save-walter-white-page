87b7342121512dd3d219caeb0e43107fa26e8120
maicondguerian <maicondguerian@gmail.com>
2022-09-14 04:14:51 -0300

    inplementando click para redirecionar

---
+++ index.js
@@ -0,0 +1,13 @@
+function clickBody(){
+   
+window.open("https://github.com/maicondguerian", "_blank")   
+    // window.location.href="https://github.com/maicondguerian"
+}
+
+window.onclick=()=>{
+    clickBody()
+}
+// window.onclick(function(){
+
+// })
+// window.onclick(clickBody)
